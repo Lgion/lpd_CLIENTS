1a43eb66d8b9f4721845cfb3db6741b0ac9767ce
athena <legion.athenienne@gmail.com>
2025-02-08 08:07:23 +0000

    WIP-10% re-adding ecommerce pages

---
+++ app/ecommerce-chretien-abidjan/_/ModalProduct.jsx
@@ -0,0 +1,51 @@
+import {useContext} from 'react'
+import Link from "next/link"
+import Image from "next/image"
+import AuthContext from "../../../stores/authContext.js"
+
+
+export default function ModalProduct({myLoader,item,img,setCartBox,option,handleAddToCart}) {
+
+    const LocalCart = () => <div className="localCart">
+        local cart
+    </div>
+    , handleChange = e => { 
+
+    }
+    
+    return <div className="toPutInModal">
+        <div className="img">
+            <Image
+                loader={myLoader}
+                src={img}
+                alt={item.fr__}
+                width={200}                                    height={200}
+            />
+        </div>
+        <div className="content">
+            <figcaption>{item.fr}</figcaption>
+            <p>{item.fr1}</p>
+            <div className="prix">{item.prix}</div>
+            <div className="modalCartContainer">
+                <LocalCart />
+                <button className="addToCart" 
+                    onClick={(e)=>{handleAddToCart(e,setCartBox, miniCart)}}
+                    data-id={item.id_produits}
+                    data-coloris={option?.coloris || ""}
+                    data-couverture={option?.couverture || ""}
+                    data-option_name={option?.opt_nom || ""}
+                    data-price={item.prix}
+                    title={"Ajouter au panier"}
+                ></button>
+            </div>
+        </div>
+
+        { option &&<div className="options">
+                    <span className="coloris">{option.coloris}</span>
+                    <span className="couverture">{option.couverture}</span>
+                    <span className="option_name">{option.opt_nom}</span>
+                </div>
+        }
+        <input className="localQty" onChange={handleChange} defaultValue="0" type="number" min="1" max="99"/>
+    </div>
+}
