2c876309aef69e64d13091043963408f91f13108
athena <legion.athenienne@gmail.com>
2025-02-14 16:52:03 +0000

    DONE MIGRATION v15 RÉUSSI (affichage OK)

---
+++ app/ecommerce-chretien-abidjan/layout.jsx
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: 'Ecommerce Chrétien - Librairie Puissance Divine d\'Amour',
+  description: 'Publications chrétiennes et objets de piété',
+}
+
+export default function Layout({ children }) {
+  return children
+}
